3fe426b81acf5dd0a141843bcc520ea4233685c7
bak2k3 <benjamin.a.kavanagh@gmail.com>
2023-12-19 16:05:16 +0000

    feat(navbar): implement responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('src/img/hero-background.jpeg')",
+      },
+      fontFamily: {
+        yellowtail: ["Yellowtail", "cursive"],
+        caveat: ["Caveat", "cursive"],
+        arimo: ["Arimo", "sans-serif"],
+        assistant: ["Assistant", "sans-serif"],
+      },
+      colors: {
+        customWhite: "#FCF2F2",
+        softGreen: "#E8E2C3",
+        strongGreen: "#9EB375",
+        customBlue: "#3E7578",
+        customBlack: "#1B1C1B",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
