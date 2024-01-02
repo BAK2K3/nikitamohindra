8b8c564f6009717cdcfab9bd4ccebaf0c812b761
bak2k3 <benjamin.a.kavanagh@gmail.com>
2024-01-02 09:32:00 +0000

    feat(col): update styling for viewer

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const gallerySettings = {
+    toolbar: {
+      zoomIn: 1,
+      zoomOut: 1,
+      prev: 1,
+      next: 1,
+      rotateLeft: 4,
+      rotateRight: 4,
+      flipHorizontal: 4,
+      flipVertical: 4,
+    },
+    title: [true, (image) => `${image.alt}`],
+    transition: true,
+  };
+
   const makingWavesGallery = new Viewer(
-    document.getElementById("makingWavesImages")
+    document.getElementById("makingWavesImages"),
+    gallerySettings
   );
 
   document
