947e6b5f9f66a14de6c29d510a6bb7776f7abd98
bak2k3 <benjamin.a.kavanagh@gmail.com>
2024-01-06 17:34:41 +0000

    fix(contact): revert to old resp for contact

--- src/js/container_resize.js
+++ src/js/container_resize.js
@@ -8,6 +8,8 @@ function adjustFixedHeightDiv() {
   const homeBackgroundElement = document.getElementById("homeBackground");
   const homeBackgroundElement2 = document.getElementById("homeBackground2");
 
+  const contactElement = document.getElementById("contactSection");
+
   if (aboutVideoElement && aboutbodyElement) {
     aboutVideoElement.style.height = windowHeight - navbarHeight + "px";
     aboutbodyElement.style.height = windowHeight - navbarHeight + "px";
@@ -16,6 +18,10 @@ function adjustFixedHeightDiv() {
   if (homeBackgroundElement && homeBackgroundElement2) {
     homeBackgroundElement.style.height = windowHeight - navbarHeight + "px";
     homeBackgroundElement2.style.height = windowHeight - navbarHeight + "px";
+  }
+
+  if (contactElement) {
+    contactElement.style.height = windowHeight - navbarHeight + "px";
   }
 }
 
