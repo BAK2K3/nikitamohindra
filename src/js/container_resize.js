e43b394a8bd613cad14c53fac293f73dc9331085
bak2k3 <benjamin.a.kavanagh@gmail.com>
2024-01-06 15:53:45 +0000

    temp: update branch

--- src/js/container_resize.js
+++ src/js/container_resize.js
@@ -1,36 +1,36 @@
-// Adjust the height of the fixed-height div
-function adjustFixedHeightDiv() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+// // Adjust the height of the fixed-height div
+// function adjustFixedHeightDiv() {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-  const windowHeight =
-    visualViewport.height ||
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.body.clientHeight;
-  const navbarHeight = document.getElementById("navbar").offsetHeight;
+//   const windowHeight =
+//     visualViewport.height ||
+//     window.innerHeight ||
+//     document.documentElement.clientHeight ||
+//     document.body.clientHeight;
+//   const navbarHeight = document.getElementById("navbar").offsetHeight;
 
-  const aboutVideoElement = document.getElementById("aboutMeVideo");
-  const aboutbodyElement = document.getElementById("aboutMeBody");
-  const homeBackgroundElement = document.getElementById("homeBackground");
-  const homeBackgroundElement2 = document.getElementById("homeBackground2");
-  const contactSectionElement = document.getElementById("contactSection");
+//   const aboutVideoElement = document.getElementById("aboutMeVideo");
+//   const aboutbodyElement = document.getElementById("aboutMeBody");
+//   const homeBackgroundElement = document.getElementById("homeBackground");
+//   const homeBackgroundElement2 = document.getElementById("homeBackground2");
+//   const contactSectionElement = document.getElementById("contactSection");
 
-  if (aboutVideoElement && aboutbodyElement) {
-    aboutVideoElement.style.height = windowHeight - navbarHeight + "px";
-    aboutbodyElement.style.height = windowHeight - navbarHeight + "px";
-  }
+//   if (aboutVideoElement && aboutbodyElement) {
+//     aboutVideoElement.style.height = windowHeight - navbarHeight + "px";
+//     aboutbodyElement.style.height = windowHeight - navbarHeight + "px";
+//   }
 
-  if (homeBackgroundElement && homeBackgroundElement2) {
-    homeBackgroundElement.style.height = windowHeight - navbarHeight + "px";
-    homeBackgroundElement2.style.height = windowHeight - navbarHeight + "px";
-  }
+//   if (homeBackgroundElement && homeBackgroundElement2) {
+//     homeBackgroundElement.style.height = windowHeight - navbarHeight + "px";
+//     homeBackgroundElement2.style.height = windowHeight - navbarHeight + "px";
+//   }
 
-  if (contactSectionElement) {
-    contactSectionElement.style.height = windowHeight - navbarHeight + "px";
-  }
-}
+//   if (contactSectionElement) {
+//     contactSectionElement.style.height = windowHeight - navbarHeight + "px";
+//   }
+// }
 
-// Initial adjustment and listen for window resize
-adjustFixedHeightDiv();
-window.addEventListener("resize", adjustFixedHeightDiv);
+// // Initial adjustment and listen for window resize
+// adjustFixedHeightDiv();
+// window.addEventListener("resize", adjustFixedHeightDiv);
